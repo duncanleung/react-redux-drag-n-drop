32b7005c678eb7fbd69649ed0836811086d0d914
Duncan L <leungd@gmail.com>
2017-09-13 14:58:22 -0700

    Creates Base Chessboard Styles

--- knight-chessboard/src/index.js
+++ knight-chessboard/src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import { render } from "react-dom";
 
-import App from "./App";
+import styled from "styled-components";
 
-render(<App />, document.getElementById("root"));
+import Board from "./components/Board";
+
+const knightPosition = [7, 4];
+
+const ChessBoard = styled.div`height: 300px;`;
+
+render(
+  <ChessBoard>
+    <Board knightPosition={knightPosition} />
+  </ChessBoard>,
+  document.getElementById("root")
+);
